720c19c4515277608d0524083d6a9d1a7294f6ff
quadribola91 <146897906+quadribola91@users.noreply.github.com>
2024-01-28 18:30:06 +0100

    Update blog.js

--- src/blog.js
+++ src/blog.js
@@ -1,12 +1,10 @@
 export const blogs = {
   date: "3rd December 2023",
-  title: "Jr. React FrontEnd Developer",
+  title: "FrontEnd Developer",
   body: "You can know it",
   name: "I'M Omobolarinwa Quadri",
   content: `A passionate a motivated and adaptable Junior Front-end Developer with a love for crafting beautiful
    and user-friendly digital experiences to join your team or collaborate on exciting projects, let's connect!
-    I'm excited about the opportunities that lie ahead, and I can't wait to bring my skills and enthusiasm to 
-    the forefront of the tech industry.
              Master Neutral Intelligence Section`,
   prop: "About Me",
   project: "Let's build the future together! 🚀",
