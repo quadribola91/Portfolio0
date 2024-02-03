9997f6a8b84d4155145eb72a6d50e58841a78bc1
quadribola91 <146897906+quadribola91@users.noreply.github.com>
2024-02-03 23:46:03 +0100

    Update ProjectsPage.js

--- src/ProjectsPage.js
+++ src/ProjectsPage.js
@@ -8,7 +8,7 @@ const ProjectsPage = () => {
   return (
     <div>
       <div className="container mx-auto mt-12">
-        <h1 className="text-3xl font-bold mb-6">Live Projects</h1>
+        <h1 className="text-3xl font-bold mb-6 mt-8 p-4">Live Projects</h1>
         <LifeProjects />
         {/* Add other content or components as needed */}
       </div>
