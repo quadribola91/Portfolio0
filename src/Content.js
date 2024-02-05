e1a5e2e37b76e493c7f23ef7179db6044cb7950d
quadribola91 <146897906+quadribola91@users.noreply.github.com>
2024-02-06 00:04:59 +0100

    Update Content.js

--- src/Content.js
+++ src/Content.js
@@ -3,11 +3,14 @@ import pic from "./ProfilePic.jpg";
 import AboutMe from "./AboutMe";
 import Contact from "./Contact";
 import ProjectsPage from "./ProjectsPage";
+import Articles from "./Articles";
+import Stories from "./Stories";
 
 export default function Content({ blog }) {
   const blogNameRef = useRef(null);
   const blogTitleRef = useRef(null);
   const projectRef = useRef(null);
+  const contentRef = useRef(null);
 
   const [theme, setTheme] = useState("light"); // Default theme is light
 
@@ -52,7 +55,15 @@ export default function Content({ blog }) {
 
     changeStyle(blogNameRef);
     changeStyle(blogTitleRef);
+    toggleVisibility();
 
+    // Apply scrolling animation to contentRef
+    const contentElement = contentRef.current;
+    if (contentElement) {
+      contentElement.style.animation = "flowingMotion 5s linear infinite";
+    }
+
+    // Toggle visibility at an interval
     const intervalId = setInterval(() => {
       toggleVisibility();
     }, 2000);
@@ -62,13 +73,29 @@ export default function Content({ blog }) {
 
   return (
     <>
+      <style>
+        {`
+          @keyframes flowingMotion {
+            0% {
+              transform: translateY(0);
+            }
+            50% {
+              transform: translateY(-20%);
+            }
+            100% {
+              transform: translateY(0);
+            }
+          }
+        `}
+      </style>
+
       <div
         className={`bg-${
           theme === "light" ? "white" : "black"
         } p-6 md:p-8 rounded-lg mb-6`}
         id="Home"
       >
-        <div className="relative block overflow-hidden rounded-lg border border-gray-100 mt-8 p-4 lg:p-6 transition-transform hover:transform-hover">
+        <div className="relative block overflow-hidden rounded-lg border border-gray-100 mt-8 p-4 lg:p-6 transition-transform hover:transform-hover hover:animate-background rounded-xl bg-gradient-to-r from-white-100 via-gray-400 to-white p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s]">
           {/* Toggle Button */}
 
           <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
@@ -101,8 +128,9 @@ export default function Content({ blog }) {
             </div>
           </div>
 
-          <div className="mt-4 mb-12">
+          <div className="mt-4 mb-12 group border-s-4 border-gray-700 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden">
             <p
+              ref={contentRef}
               className={`text-lg text-${
                 theme === "light" ? "black" : "white"
               } font-bold font-mono hover:font-serif hover:text-${
@@ -125,6 +153,8 @@ export default function Content({ blog }) {
 
       {/* ... rest of your code ... */}
       <AboutMe />
+      <Articles />
+      <Stories />
       <ProjectsPage />
       <Contact />
     </>
