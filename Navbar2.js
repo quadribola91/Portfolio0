b3155b58eb8a4ad69d9498578747ef3d0fb762fc
quadribola91 <146897906+quadribola91@users.noreply.github.com>
2024-02-13 18:14:20 +0100

    Update Navbar2.js

--- Navbar2.js
+++ Navbar2.js
@@ -1,173 +1,145 @@
-// Navbar2.js
-import React, { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
-import logo1 from "./pictures/highC.jpeg";
-import "./Navbar2.css"; // Import the CSS file for styling
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo1 from "./ProfilePic.jpg";
 
-function Navbar2() {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-  const location = useLocation();
+const Navbar2 = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleNav = () => {
-    setIsNavOpen(!isNavOpen);
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
   };
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const navbar = document.getElementById("navbar");
-      const scrollPosition = window.scrollY;
-      const threshold = 200;
-
-      if (scrollPosition > threshold) {
-        navbar.classList.add("fixed-bottom");
-      } else {
-        navbar.classList.remove("fixed-bottom");
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <header className={`bg-white ${isNavOpen ? "md:hidden" : ""}`} id="navbar">
-      <div className="bg-white container fixed top-0 z-50 overflow-hidden mt-0 mb-8 mx-auto top-0 flex items-center  justify-between p-1">
-        {/* Container for logo */}
-        <div className="flex items-center">
-          <img src={logo1} className="rounded-full h-12" alt="Logo" />
+    <div className="bg-white top-0 left-0 w-full z-50 fixed">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between h-16">
+          <div className="flex">
+            <div className="flex-shrink-0 flex items-center">
+              <Link to="/" className="text-white">
+                <img
+                  src={logo1}
+                  alt="Logo"
+                  className="w-12 h-12 rounded-full"
+                />
+              </Link>
+            </div>
+          </div>
+          <div className="flex">
+            <div className="hidden lg:block mt-4">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <Link
+                  to="/"
+                  className="text-gray-800 font-bold hover:text-gray-300"
+                >
+                  Home
+                </Link>
+                <Link
+                  to="/Home"
+                  className="text-gray-800 font-bold hover:text-gray-300"
+                >
+                  About
+                </Link>
+                <Link
+                  to="/contact"
+                  className="text-gray-800 font-bold hover:text-gray-300"
+                >
+                  Contact
+                </Link>
+                <button className="bg-blue-500 hover:bg-blue-200 ml-10 p-2 rounded-full ">
+                  <Link
+                    to="/projects"
+                    className="text-white hover:text-white font-bold text-mono"
+                  >
+                    Projects
+                  </Link>
+                </button>
+              </div>
+            </div>
+            <div className="lg:hidden">
+              <button
+                onClick={handleToggle}
+                className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {/* Icon when menu is closed */}
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  // Icon when menu is open
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
         </div>
+      </div>
 
-        {/* Container for middle components */}
-        <div
-          className={`hidden md:flex items-center space-x-4  ${
-            isNavOpen ? "block" : "hidden"
-          }`}
-        >
-          <div className="nav-center bg-white p-4 rounded-full shadow-lg">
+      {/* Mobile menu */}
+      {isOpen && (
+        <div className="lg:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <Link
-              to="/home"
-              className={`text-black hover:text-gray-700 font-mono hover:font-sans font-bold m-2 ${
-                location.pathname === "/Home" ? "hover:text-green-500" : ""
-              } transition`}
+              to="/"
+              className="text-gray-600 font-bold hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base"
             >
               Home
             </Link>
+
             <Link
-              to="/AboutMe"
-              className={`text-black hover:text-gray-700 font-mono hover:font-sans font-bold m-2 ${
-                location.pathname === "/AboutMe" ? "hover:text-green-500" : ""
-              } transition`}
+              to="/Home"
+              className="text-gray-600 font-bold hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base"
             >
-              AboutMe
+              About
             </Link>
+
             <Link
               to="/contact"
-              className={`text-black hover:text-gray-700 font-mono hover:font-sans font-bold m-2 ${
-                location.pathname === "/contact" ? "hover:text-green-500" : ""
-              } transition`}
+              className="text-gray-600 font-bold hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base"
             >
               Contact
             </Link>
+            <button className="bg-blue-700 mt-6 p-2 rounded-full hover:bg-blue-300 px-3 py-2">
+              <Link
+                to="/projects"
+                className=" text-white hover:text-white font-bold block text-mono "
+              >
+                Projects
+              </Link>
+            </button>
           </div>
         </div>
-
-        {/* Container for Sign Agreement */}
-        <div
-          className={`hidden md:flex items-center space-x-4 ${
-            isNavOpen ? "hidden" : "block"
-          }`}
-        >
-          <div className="nav-center bg-red p-2 rounded-full shadow-lg">
-            <Link
-              to="/projects"
-              className={`text-black hover:text-red-800 text-xl font-mono hover:font-sans font-bold m-2 font-bold m-2 ${
-                location.pathname === "/projects" ? "hover:text-red-500" : ""
-              } transition`}
-            >
-              Projects
-            </Link>
-            {/* Add other navigation links as needed */}
-          </div>
-        </div>
-
-        {/* Toggle button for mobile */}
-        <div className="md:hidden">
-          <button onClick={toggleNav} className="focus:outline-none">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={`h-6 w-6 ${isNavOpen ? "hidden" : "block"}`}
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              ></path>
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={`h-6 w-6 ${isNavOpen ? "block" : "hidden"}`}
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              ></path>
-            </svg>
-          </button>
-        </div>
-      </div>
-
-      {/* Adjusted styles for more space, increased width, and reduced padding */}
-      <div className={`md:hidden ${isNavOpen ? "block" : "hidden"} px-6`}>
-        <div className="flex flex-col items-center space-y-4 mt-8 p-4">
-          <Link
-            to="/Home"
-            className={`text-black hover:text-gray-700 text-xl font-mono hover:font-sans font-bold m-2 transition ${
-              location.pathname === "/Home" ? "active" : ""
-            }`}
-          >
-            Home
-          </Link>
-          <Link
-            to="/AboutMe"
-            className={`text-black hover:text-gray-700 text-xl font-mono hover:font-sans font-bold m-2 transition ${
-              location.pathname === "/AboutMe" ? "active" : ""
-            }`}
-          >
-            AboutMe
-          </Link>
-          <Link
-            to="/contact"
-            className={`text-black hover:text-gray-700 text-xl font-mono hover:font-sans font-bold m-2 transition ${
-              location.pathname === "/contact" ? "active" : ""
-            }`}
-          >
-            Contact
-          </Link>
-          <Link
-            to="/projects"
-            className={`bg-blue-800 hover:bg-blue-200 text-xl hover:text-green-800 text-white rounded-full px-2 py-2 font-medium ${
-              location.pathname === "/projects" ? "active" : ""
-            }`}
-          >
-            Projects
-          </Link>
-          {/* Sign Agreement button is intentionally omitted in mobile view */}
-        </div>
-      </div>
-    </header>
+      )}
+    </div>
   );
-}
+};
 
 export default Navbar2;
