1b1a155521835e2013bf9c8a022c33d65d903d17
quadribola91 <146897906+quadribola91@users.noreply.github.com>
2024-02-13 17:58:59 +0100

    Update AboutMe.js

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from "react";
-import H1 from "./pictures/Html1.png";
-import C1 from "./pictures/Css1.png";
-import J1 from "./pictures/Javascript1.png";
-import T1 from "./pictures/Tailwind1.jpeg";
-import R1 from "./pictures/React1.jpeg";
+import { FiPhoneCall, FiGlobe, FiMapPin } from "react-icons/fi"; // Import icons from react-icons
 import CVDownloadButton from "./CVDownloadbutton";
-import About from "./About.js";
 import Advanceflex from "./Advanceflex.js";
 
 const AboutMe = () => {
@@ -31,114 +26,92 @@ const AboutMe = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-  const images = [
-    { image: H1, skill: "HTML5" },
-    { image: C1, skill: "CSS3" },
-    { image: J1, skill: "JavaScript (ES6+)" },
-    { image: T1, skill: "Tailwind CSS" },
-    { image: R1, skill: "React.js" },
-  ];
-
   return (
     <>
-      <div
-        id="AboutMe"
-        className={`bg-${
-          theme === "light" ? "white" : "black"
-        } hover:animate-background rounded-xl bg-gradient-to-r from-white-100 via-gray-400 to-white p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s] text-${
-          theme === "light" ? "black" : "white"
-        }`}
-      >
-        <h1 className="ml-8 mt-4 text-gray-800 font-bold font-mono h-8 text-5xl hover:font-sans hover:text-gray-600">
-          DATA
-        </h1>
-        <div className="about-me-container  flex flex-col md:flex-row items-center md:divide-x md:divide-gray-300 p-8 justify-content: space-between">
-          {/* Personal Information Section */}
-          <div
-            className={`personal-info-section md:mr-8 w-full md:w-1/2 lg:max-w-md p-6 border rounded-lg ${
-              theme === "light" ? "bg-white-300" : "bg-gray-800"
-            } shadow-md`}
-          >
-            <h2
-              className={`text-2xl font-bold mb-2 text-center ${
-                theme === "light" ? "text-black" : "text-gray-800"
-              }`}
+      <div className="flex flex-col lg:flex-row md:h-half p-4">
+        <div
+          id="aboutMe"
+          className={`bg-${
+            theme === "light" ? "white" : "black"
+          } hover:animate-background md:w-half md:h-half rounded-xl bg-gradient-to-r from-white-100 via-gray-400 to-white p-2 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s] text-${
+            theme === "light" ? "black" : "white"
+          }`}
+        >
+          <div className="about-me-container  flex flex-col md:flex-row  items-center md:divide-x md:divide-gray-300 p-8 justify-content: space-between">
+            {/* Personal Information Section */}
+            <div
+              className={`personal-info-section md:mr-8 w-full md:h-1/2 lg:max-w-md p-6 border rounded-lg ${
+                theme === "light" ? "bg-white-300" : "bg-gray-800"
+              } shadow-md`}
             >
-              Personal
-            </h2>
-            <div className="border-4 border-gray-500 rounded-lg p-4 shadow-md mb-4 end">
-              <div id="sparko">
-                <p
-                  className={`text-lg ${
-                    theme === "light" ? "text-gray-700" : "text-gray-300"
-                  } mb-4 font-mono hover:font-sans`}
-                >
-                  Hi, I'm Omobolarinwa Quadri, a passionate front-end developer
-                  dedicated to creating engaging and user-friendly web
-                  experiences.
-                </p>
+              <h2
+                className={`text-2xl font-bold mb-2 text-center ${
+                  theme === "light" ? "text-black" : "text-pink-800"
+                }`}
+              >
+                Personal
+              </h2>
+              <div className="border-2 border-gray-900 rounded-md md p-4 shadow-xl mb-4 end">
+                <div id="sparko">
+                  <p
+                    className={`text-lg ${
+                      theme === "light" ? "text-gray-700" : "text-gray-300"
+                    } mb-4 font-mono hover:font-sans`}
+                  >
+                    Hi, I'm Omobolarinwa Quadri, a passionate front-end
+                    developer dedicated to creating engaging and user-friendly
+                    web experiences.
+                  </p>
+                </div>
+                <div id="lighto">
+                  {/* Content for the lighto section (if any) */}
+                </div>
               </div>
-              <div id="lighto">
-                {/* Content for the lighto section (if any) */}
+              <div
+                className="flex flex-col md:flex-row md:justify-center gap-4"
+                id="brighto"
+              >
+                {/* Replace the border divs with icon divs */}
+                <div className="mt-8 md:mt-0">
+                  <a
+                    href="tel:+2349058824643"
+                    className="flex items-center justify-center"
+                  >
+                    <FiPhoneCall size={35} className="mr-2" />{" "}
+                    {/* Phone icon */}
+                    <p
+                      className={`text-md font-bold ${
+                        theme === "light" ? "text-green-600" : "text-gray-300"
+                      }`}
+                    >
+                      +2349058824643
+                    </p>
+                  </a>
+                </div>
+                <div className="flex items-center justify-center ">
+                  <FiMapPin size={24} className="mr-2" />{" "}
+                  {/* Nationality icon */}
+                  <p className="text-md font-bold text-green-600">Nigerian</p>
+                </div>
+                <div className="flex items-center justify-center">
+                  <FiGlobe size={24} className="mr-2" /> {/* Language icon */}
+                  <p className="text-md font-bold text-green-600">English</p>
+                </div>
+              </div>
+              <div className="flex justify-center mt-4">
+                {" "}
+                {/* Center the button */}
+                <CVDownloadButton />
               </div>
             </div>
-            <div className="grid grid-cols-2 gap-4" id="brighto">
-              <div className="border-4 border-gray-500 rounded-lg p-4 shadow-md mb-4">
-                <p
-                  className={`text-lg ${
-                    theme === "light" ? "text-gray-600" : "text-gray-300"
-                  }`}
-                >
-                  <span className="font-bold text-black">Age:</span> {age}
-                </p>
-                <p className="text-sm text-green-600">
-                  <span className="font-bold text-black">Freelance:</span>{" "}
-                  Available
-                </p>
-                <p className="text-sm text-green-600">
-                  <span className="font-bold text-black">Remote:</span>{" "}
-                  Available
-                </p>
-                <p className="text-sm text-green-600">
-                  <span className="font-bold text-black">Hybrid:</span>{" "}
-                  Available
-                </p>
-              </div>
-              <div className="border-4 border-gray-500 rounded-lg p-2 shadow-md mb-4">
-                <p
-                  className={`text-sm ${
-                    theme === "light" ? "text-green-600" : "text-gray-300"
-                  }`}
-                >
-                  <span className="font-bold text-black">Phone:</span>{" "}
-                  +2349058824643
-                </p>
-                <p className="text-sm text-green-600">
-                  <span className="font-bold text-black">Nationality:</span>{" "}
-                  Nigerian
-                </p>
-                <p className="text-sm text-green-600">
-                  <span className="font-bold text-black">Languages:</span>{" "}
-                  English
-                </p>
-              </div>
-            </div>
-            <div>
-              <CVDownloadButton />
-            </div>
-          </div>
 
-          {/* Experience and Education Section */}
-          <div
-            className={`experience-education-section w-full md:w-1/2 justify-center mt-10 mr-10 lg:max-w-md p-6 border rounded-lg shadow-md ml-20 ${
-              theme === "light" ? "bg-white" : "bg-gray-800"
-            }`}
-          >
-            <About />
+            {/* Experience and Education Section */}
           </div>
         </div>
+        <div className="block p-8 md:w-half md:h-half shadow-xl">
+          <Advanceflex />
+        </div>
       </div>
-      <Advanceflex />
     </>
   );
 };
