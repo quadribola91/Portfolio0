1b7ae0ff340ba93b4dba146874afc3eb1d659366
quadribola91 <146897906+quadribola91@users.noreply.github.com>
2024-02-13 21:26:44 +0100

    Update Navbar2.js

--- src/Navbar2.js
+++ src/Navbar2.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo1 from "./ProfilePic.jpg";
 
-const Navbar2 = () => {
+const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleToggle = () => {
@@ -10,8 +10,12 @@ const Navbar2 = () => {
   };
 
   return (
-    <div className="bg-white top-0 left-0 w-full z-50 fixed">
-      <div className="max-w-7xl mx-auto px-4">
+    <div className="bg-white top-0 left-0 w-full z-50 fixed w-1/2 lg:w-full">
+      <div
+        className={`max-w-7xl mx-auto px-4 ${
+          isOpen ? "block" : "hidden"
+        } lg:block`}
+      >
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
@@ -24,79 +28,33 @@ const Navbar2 = () => {
               </Link>
             </div>
           </div>
-          <div className="flex">
-            <div className="hidden lg:block mt-4">
-              <div className="ml-10 flex items-baseline space-x-4">
-                <Link
-                  to="/"
-                  className="text-gray-800 font-bold hover:text-gray-300"
-                >
-                  Home
-                </Link>
-                <Link
-                  to="/Home"
-                  className="text-gray-800 font-bold hover:text-gray-300"
-                >
-                  About
-                </Link>
-                <Link
-                  to="/contact"
-                  className="text-gray-800 font-bold hover:text-gray-300"
-                >
-                  Contact
-                </Link>
-                <button className="bg-blue-500 hover:bg-blue-200 ml-10 p-2 rounded-full ">
-                  <Link
-                    to="/projects"
-                    className="text-white hover:text-white font-bold text-mono"
-                  >
-                    Projects
-                  </Link>
-                </button>
-              </div>
-            </div>
-            <div className="lg:hidden">
-              <button
-                onClick={handleToggle}
-                className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-expanded="false"
+          <div className="hidden lg:block mt-4">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <Link
+                to="/"
+                className="text-gray-800 font-bold hover:text-gray-300"
               >
-                <span className="sr-only">Open main menu</span>
-                {/* Icon when menu is closed */}
-                {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                ) : (
-                  // Icon when menu is open
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
+                Home
+              </Link>
+              <Link
+                to="/Home"
+                className="text-gray-800 font-bold hover:text-gray-300"
+              >
+                About
+              </Link>
+              <Link
+                to="/contact"
+                className="text-gray-800 font-bold hover:text-gray-300"
+              >
+                Contact
+              </Link>
+              <button className="bg-blue-500 hover:bg-blue-200 ml-10 p-2 rounded-full ">
+                <Link
+                  to="/projects"
+                  className="text-white hover:text-white font-bold text-mono"
+                >
+                  Projects
+                </Link>
               </button>
             </div>
           </div>
@@ -104,9 +62,51 @@ const Navbar2 = () => {
       </div>
 
       {/* Mobile menu */}
-      {isOpen && (
-        <div className="lg:hidden" id="mobile-menu">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+      <div className="lg:hidden">
+        <button
+          onClick={handleToggle}
+          className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          {/* Icon when menu is closed */}
+          {!isOpen ? (
+            <svg
+              className="block h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          ) : (
+            // Icon when menu is open
+            <svg
+              className="block h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          )}
+        </button>
+        {isOpen && (
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 w-1/2 lg:w-full">
             <Link
               to="/"
               className="text-gray-600 font-bold hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base"
@@ -136,10 +136,10 @@ const Navbar2 = () => {
               </Link>
             </button>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
 
-export default Navbar2;
+export default Navbar;
